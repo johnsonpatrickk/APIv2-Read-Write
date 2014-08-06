09b3ee062dca36c44ce20daa2dd87918c6885751
Patrick K. Johnson Jr <pjohnson@coeustec.com>
2014-08-06 15:57:51 -0400

    modified admin controller [APIv2-AdminUI]

--- adminui/controllers/access_control/admin.php
+++ adminui/controllers/access_control/admin.php
@@ -1,7 +1,4 @@
-<?php
-
-if (! defined ( "BASEPATH" ))
-	exit ( "No direct script access allowed" );
+<?php if (! defined ( "BASEPATH" )) exit ( "No direct script access allowed" );
 
 /**
  * APIv2 AdminUI Pilot Admin Controller
